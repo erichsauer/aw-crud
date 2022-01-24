b543667ca7d69931371ad8c3eb4f8570c8461b42
Erich Sauer <erich.sauer@gmail.com>
2022-01-24 11:24:03 -0800

    dog controllers and models built

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS dogs;
+
+CREATE TABLE dogs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  age INT NOT NULL,
+  favorite_treat TEXT NOT NULL
+)
